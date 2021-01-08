ab1934469f1ccef88780c301ce621ed9420c5fac
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-01-08 16:58:15 -0300

    Burger.js ahora pasa de manera dinamica los ingredientes

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import React from "react";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    (eachKey) => {
+      return [...Array(props.ingredients[eachKey])].map((_, index) => {
+        return <BurgerIngredient key={eachKey + index} type={eachKey} />;
+      });
+    }
+  );
+
   return (
     <div className="Burger">
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
