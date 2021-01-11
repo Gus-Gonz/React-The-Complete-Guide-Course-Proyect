891316101ee3523c21a6b928f9dd9211487b99de
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-01-11 14:05:03 -0300

    Backdrop cierra el modal

--- src/components/Ui/Backdrop/Backdrop.js
+++ src/components/Ui/Backdrop/Backdrop.js
@@ -1,5 +1,6 @@
 import React from "react";
 
-const backdrop = (props) => (props.show ? <div></div> : null);
+const backdrop = (props) =>
+  props.show ? <div className="Backdrop" onClick={props.clicked}></div> : null;
 
 export default backdrop;
--- src/components/Ui/Modal/Modal.js
+++ src/components/Ui/Modal/Modal.js
@@ -1,14 +1,20 @@
 import React from "react";
 
+import Aux from "../../../hoc/Auxilary";
+import Backdrop from "../Backdrop/Backdrop";
+
 const modal = (props) => (
-  <div
-    className="Modal"
-    style={{
-      opacity: props.show ? "1" : "0",
-      transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-    }}>
-    {props.children}
-  </div>
+  <Aux>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div
+      className="Modal"
+      style={{
+        opacity: props.show ? "1" : "0",
+        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+      }}>
+      {props.children}
+    </div>
+  </Aux>
 );
 
 export default modal;
