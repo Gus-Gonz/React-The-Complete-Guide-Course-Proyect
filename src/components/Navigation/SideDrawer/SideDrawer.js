7062f3960f46f315e752e3ded7c23f3942aedd0c
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-01-15 11:02:34 -0300

    Se cambio la posicion de del layout y del auxilary

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavItems from "../NavItems/NavItems";
 import BackDrop from "../../Ui/Backdrop/Backdrop";
-import Aux from "../../../hoc/Auxilary";
+import Aux from "../../../hoc/Auxilary/Auxilary";
 
 const sideDrawer = (props) => {
   let attachedClasses = ["SideDrawer", props.open ? "Open" : "Closed"];
--- src/components/Ui/Modal/Modal.js
+++ src/components/Ui/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import Aux from "../../../hoc/Auxilary";
+import Aux from "../../../hoc/Auxilary/Auxilary";
 import Backdrop from "../Backdrop/Backdrop";
 
 class Modal extends Component {
