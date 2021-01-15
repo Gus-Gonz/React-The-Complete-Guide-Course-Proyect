d3063d345a31a9b82ca1f7600a22f6a8e0922de7
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-01-15 10:52:14 -0300

    El modal ahora no posee problemas de performance

--- src/components/Ui/Modal/Modal.js
+++ src/components/Ui/Modal/Modal.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../../hoc/Auxilary";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className="Modal"
-      style={{
-        opacity: props.show ? "1" : "0",
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-      }}>
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  componentWillUpdate() {
+    console.log("[Model] WillUpdate");
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className="Modal"
+          style={{
+            opacity: this.props.show ? "1" : "0",
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+export default Modal;
