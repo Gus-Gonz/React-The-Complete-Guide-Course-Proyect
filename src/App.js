578141f280d15e052f2155c5c092d02463cf8cbe
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-01-28 12:16:29 -0300

    Se creo la pagina CheckOut

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/CheckOut/CheckOut";
 
 function App() {
   return (
     <div>
       <Layout>
         <BurgerBuilder></BurgerBuilder>
+        <Checkout />
       </Layout>
     </div>
   );
---
+++ src/components/Order/CheckOutSummary/CheckOutSummary.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Burger from "../../Burger/Burger";
+import Button from "../../Ui/Button/Button";
+
+import "./CheckOutSummary.css";
+
+const CheckoutSummary = (props) => {
+  return (
+    <div className={"CheckoutSummary"}>
+      <h1>We hope it taste well!!</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button clicked btnType="Danger">
+        CANCEL
+      </Button>
+      <Button clicked btnType="Success">
+        CONTINUE
+      </Button>
+    </div>
+  );
+};
+
+export default CheckoutSummary;
