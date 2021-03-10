674ea57e2328ae839650de25477ea29da5eb4d42
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-10 10:45:52 -0300

    se agrego mas pruebas  y se uso metodos como beforeEach y setProps

--- src/components/Navigation/NavItems/NavItems.test.js
+++ src/components/Navigation/NavItems/NavItems.test.js
@@ -9,8 +9,19 @@ import NavItem from "./NavItem/NavItem";
 configure({ adapter: new Adapter() });
 
 describe("<NavItems /> ", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavItems />);
+  });
+
   it("should render 2 <NavItem/> if not authenticated", () => {
-    const wrapper = shallow(<NavItems />);
     expect(wrapper.find(NavItem)).toHaveLength(2);
+  });
+
+  it("should render 3 <NavItem/> if authenticated", () => {
+    wrapper.setProps({
+      isAuth: true,
+    });
+    expect(wrapper.find(NavItem)).toHaveLength(3);
   });
 });
