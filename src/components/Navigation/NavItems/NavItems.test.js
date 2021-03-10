335912fa5929876828536c045d4104e62e5f1ed7
Gustavo Gonzalez <Gegf1234@Gmail.com>
2021-03-10 10:41:06 -0300

    se creo un test basico para practicar

---
+++ src/components/Navigation/NavItems/NavItems.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import NavItems from "./NavItems";
+import NavItem from "./NavItem/NavItem";
+
+configure({ adapter: new Adapter() });
+
+describe("<NavItems /> ", () => {
+  it("should render 2 <NavItem/> if not authenticated", () => {
+    const wrapper = shallow(<NavItems />);
+    expect(wrapper.find(NavItem)).toHaveLength(2);
+  });
+});
